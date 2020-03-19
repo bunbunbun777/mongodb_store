7c73f63a17ad4e505a9b8f37a45b942e8a7f8ab3
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2020-03-19 20:05:50 +0100

    [mongodb_server] Removed --smallfiles argument from mongo command

--- mongodb_store/scripts/mongodb_server.py
+++ mongodb_store/scripts/mongodb_server.py
@@ -110,7 +110,7 @@ class MongoServer(object):
 #            signal.signal(signal.SIGINT, signal.SIG_IGN)
 
         #cmd = ["mongod","--dbpath",self._db_path,"--port",str(self._mongo_port),"--smallfiles","--bind_ip","127.0.0.1"]
-        cmd = ["mongod","--dbpath",self._db_path,"--port",str(self._mongo_port),"--smallfiles"]
+        cmd = ["mongod","--dbpath",self._db_path,"--port",str(self._mongo_port)]
         if self.repl_set is not None:
             cmd.append("--replSet")
             cmd.append(self.repl_set)
@@ -186,4 +186,3 @@ class MongoServer(object):
 
 if __name__ == '__main__':
     server = MongoServer()
-
